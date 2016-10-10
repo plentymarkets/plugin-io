a8b5783a273b839ccf52d421ea879bace0af31a1
Felix Dausch <felix.dausch@plentymarkets.com>
2016-10-10 15:27:33 +0200

    ADD CategoryParams for typed pagination-params

---
+++ src/Builder/Category/CategoryParams.hh
@@ -0,0 +1,11 @@
+<?hh //strict
+
+namespace LayoutCore\Builder\Category;
+
+class CategoryParams
+{
+    public int $variationShowType = 1;
+    public int $itemsPerPage = 20;
+    public string $orderBy = "itemRand";
+    public string $orderByKey = "ASC";
+}
