a342815be6e5deec6e8e7a8acba965ca42d813fe
Felix Dausch <felix.dausch@plentymarkets.com>
2016-10-14 17:48:07 +0200

    ADD comments

--- src/Guards/AbstractGuard.php
+++ src/Guards/AbstractGuard.php
@@ -4,21 +4,36 @@ namespace LayoutCore\Guards;
 
 use LayoutCore\Helper\AbstractFactory;
 
+/**
+ * Class AbstractGuard
+ * @package LayoutCore\Guards
+ */
 abstract class AbstractGuard
 {
     /**
+     * Returned value will be compared with asserted value to decide if it should redirect.
      * @return mixed
      */
     protected abstract function assert();
 
-    public function assertOrRedirect( $expected, string $redirectUrl )
+    /**
+     * Redirect to given URI if expected value equals return value of AbstractGuard::assert()
+     * @param mixed     $expected               The value to compare with return value of AbstractGuard::asssert()
+     * @param string    $redirectUri            The URI to redirect to
+     */
+    public function assertOrRedirect( $expected, string $redirectUri )
     {
         if ( $this->assert() !== $expected )
         {
-            self::redirect( $redirectUrl, ["backlink" => self::getUrl()] );
+            self::redirect( $redirectUri, ["backlink" => self::getUrl()] );
         }
     }
 
+    /**
+     * Redirect to a given URI. Appends params as query string.
+     * @param string    $uri    The URI to redirect to.
+     * @param array     $params A map of params to append to URI as query string.
+     */
     public static function redirect( string $uri, array $params = [] )
     {
         $url = self::getUrl( $uri );
@@ -40,6 +55,12 @@ abstract class AbstractGuard
         exit;
     }
 
+    /**
+     * Transform a given URI to an URL by prepending used protocol and server name.
+     * Will return current URL if $uri is null
+     * @param string|null $uri 
+     * @return string
+     */
     private static function getUrl( string $uri = null )
     {
         if( $uri === null )
