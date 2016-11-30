6d240b9b028680fdbbd8d489cdd3fe9931c7268e
Dominik Meyer <dominik.meyer@plentymarkets.com>
2016-11-30 14:45:12 +0100

    UPDATE item search

--- src/Api/Resources/ItemSearchResource.php
+++ src/Api/Resources/ItemSearchResource.php
@@ -33,26 +33,35 @@ class ItemSearchResource extends ApiResource
      */
     public function index():BaseResponse
     {
-        $searchString = $this->request->get('searchString');
-        $page         = $this->request->get('page');
+        $searchString = $this->request->get('searchString', '');
+        
+        if(strlen($searchString))
+        {
+            $page         = $this->request->get('page', 1);
     
-        $params = [
-            'itemsPerPage' => $this->request->get('itemsPerPage'),
-            'orderBy'      => $this->request->get('orderBy'),
-            'orderByKey'   => $this->request->get('orderByKey')
-        ];
+            $params = [
+                'itemsPerPage' => $this->request->get('itemsPerPage', 20),
+                'orderBy'      => $this->request->get('orderBy', 'itemName'),
+                'orderByKey'   => $this->request->get('orderByKey', 'ASC')
+            ];
     
-        /**
-         * @var CategoryParamsBuilder $categoryParamsBuilder
-         */
-        $categoryParamsBuilder = pluginApp(CategoryParamsBuilder::class);
-        /**
-         * @var ItemService $itemService
-         */
-        $itemService           = pluginApp(ItemService::class);
+            /**
+             * @var CategoryParamsBuilder $categoryParamsBuilder
+             */
+            $categoryParamsBuilder = pluginApp(CategoryParamsBuilder::class);
+            /**
+             * @var ItemService $itemService
+             */
+            $itemService           = pluginApp(ItemService::class);
     
-        $response = $itemService->searchItems($searchString, $categoryParamsBuilder->fromArray($params), $page);
+            $response = $itemService->searchItems($searchString, $categoryParamsBuilder->fromArray($params), $page);
     
-        return $this->response->create($response, ResponseCode::OK);
+            return $this->response->create($response, ResponseCode::OK);
+        }
+        else
+        {
+            return $this->response->error(1, '');
+        }
+        
     }
 }
