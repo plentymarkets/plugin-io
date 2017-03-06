e97376ca8110f2212a318892b53d1cb5451670cb
Dominik Meyer <dominik.meyer@plentymarkets.com>
2017-03-06 09:43:00 +0100

    ADD template rest call

---
+++ src/Api/Resources/TemplateResource.php
@@ -0,0 +1,49 @@
+<?php //strict
+
+namespace IO\Api\Resources;
+
+use Plenty\Plugin\Templates\Twig;
+use Plenty\Plugin\Http\Response;
+use Plenty\Plugin\Http\Request;
+use IO\Api\ApiResource;
+use IO\Api\ApiResponse;
+use IO\Api\ResponseCode;
+
+
+/**
+ * Class TemplateResource
+ * @package IO\Api\Resources
+ */
+class TemplateResource extends ApiResource
+{
+    /**
+     * TemplateResource constructor.
+     * @param Request $request
+     * @param ApiResponse $response
+     */
+    public function __construct(Request $request, ApiResponse $response)
+    {
+        parent::__construct($request, $response);
+    }
+    
+    /**
+     * Return the given rendered template
+     * @return Response
+     */
+    public function index():Response
+    {
+        $template         = $this->request->get('template', '');
+        $renderedTemplate = '';
+        
+        if (strlen($template)) {
+            /**
+             * @var Twig $twig
+             */
+            $twig             = pluginApp(Twig::class);
+            $renderedTemplate = $twig->render($template);
+        }
+        
+        return $this->response->create($renderedTemplate, ResponseCode::OK);
+    }
+    
+}
