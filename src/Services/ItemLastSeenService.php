85f4180f199033cd2a6c6d48c89a7fd8b648bc6b
Dominik Meyer <dominik.meyer@plentymarkets.com>
2017-05-04 13:36:43 +0200

    UPDATE

--- src/Services/ItemLastSeenService.php
+++ src/Services/ItemLastSeenService.php
@@ -5,21 +5,35 @@ namespace IO\Services;
 use IO\Constants\SessionStorageKeys;
 use IO\Services\SessionStorageService;
 
+/**
+ * Class ItemLastSeenService
+ * @package IO\Services
+ */
 class ItemLastSeenService
 {
     const MAX_COUNT = 9;
     private $sessionStorage;
     
+    /**
+     * ItemLastSeenService constructor.
+     * @param \IO\Services\SessionStorageService $sessionStorage
+     */
     public function __construct(SessionStorageService $sessionStorage)
     {
         $this->sessionStorage = $sessionStorage;
     }
     
+    /**
+     * @param int $maxCount
+     */
     public function setLastSeenMaxCount(int $maxCount)
     {
         $this->sessionStorage->setSessionValue(SessionStorageKeys::LAST_SEEN_MAX_COUNT, $maxCount);
     }
     
+    /**
+     * @param int $variationId
+     */
     public function setLastSeenItem(int $variationId)
     {
         $maxCount = $this->sessionStorage->getSessionValue(SessionStorageKeys::LAST_SEEN_MAX_COUNT);
