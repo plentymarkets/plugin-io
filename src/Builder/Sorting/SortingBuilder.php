7f536efccc7ac5f56e70314946a1814356e0a32e
Christine Kolb <christine.kolb@plentymarkets.com>
2017-05-24 14:27:27 +0200

    ADD Default Sorting

--- src/Builder/Sorting/SortingBuilder.php
+++ src/Builder/Sorting/SortingBuilder.php
@@ -2,6 +2,8 @@
 
 namespace IO\Builder\Sorting;
 
+use IO\Services\TemplateConfigService;
+use Plenty\Modules\Cloud\ElasticSearch\Lib\Sorting\SingleSorting;
 use Plenty\Modules\Cloud\ElasticSearch\Lib\Sorting\SortingInterface;
 use Plenty\Modules\Cloud\ElasticSearch\Lib\Sorting\MultipleSorting;
 use Plenty\Modules\Item\Search\Sort\NameSorting;
@@ -11,24 +13,72 @@ class SortingBuilder
 {
     public static function buildSorting($sortingString)
     {
-        $e = explode('_', $sortingString);
-        
-        $sortingOrder = $e[count($e)-1];
-        $sortingPath  = str_replace('_'.$sortingOrder, '', $sortingString);
-    
+        $sortingParameter =  self::filterSortingString($sortingString);
+
         if(strpos($sortingString, 'texts.name') !== false)
         {
-            $sortingInterface = pluginApp(NameSorting::class, [self::buildNameSorting($sortingPath), pluginApp(SessionStorageService::class)->getLang(), $sortingOrder]);
+            $sortingInterface = pluginApp(NameSorting::class, [self::buildNameSorting($sortingParameter["sortingPath"]), pluginApp(SessionStorageService::class)->getLang(), $sortingParameter["sortingOrder"]]);
         }
         else
         {
             $sortingInterface = pluginApp(MultipleSorting::class);
-            $sortingInterface->add($sortingPath, $sortingOrder);
+            $sortingInterface->add($sortingParameter["sortingPath"], $sortingParameter["sortingOrder"]);
         }
-        
+
         return $sortingInterface;
     }
-    
+
+    public static function buildDefaultSorting()
+    {
+        /**
+         * @var TemplateConfigService $templateConfigService
+         */
+        $templateConfigService = pluginApp(TemplateConfigService::class);
+        $usedSortingPriority1 = $templateConfigService->get('sorting.priority1');
+        $usedSortingPriority2 = $templateConfigService->get('sorting.priority2');
+        $usedSortingPriority3 = $templateConfigService->get('sorting.priority3');
+
+        if($usedSortingPriority2 == 'item.notSelected' && $usedSortingPriority3 == 'item.notSelected')
+        {
+            //SingleSort
+            $sortingParameter1 = self::filterSortingString($usedSortingPriority1);
+            $sortingInterface = pluginApp(SingleSorting::class);
+
+        }
+        else
+        {
+            //MultiSort
+            $sortingInterface = pluginApp(MultipleSorting::class);
+
+            $sortingParameter1 = self::filterSortingString($usedSortingPriority1);
+            $sortingInterface->add($sortingParameter1["sortingPath"], $sortingParameter1["sortingOrder"]);
+            if($usedSortingPriority2 != 'item.notSelected')
+            {
+                $sortingParameter2 = self::filterSortingString($usedSortingPriority2);
+                $sortingInterface->add($sortingParameter2["sortingPath"], $sortingParameter2["sortingOrder"]);
+            }
+            if($usedSortingPriority3 != 'item.notSelected')
+            {
+                $sortingParameter3 = self::filterSortingString($usedSortingPriority3);
+                $sortingInterface->add($sortingParameter3["sortingPath"], $sortingParameter3["sortingOrder"]);
+            }
+        }
+
+        return $sortingInterface;
+    }
+
+    private static function filterSortingString($sortingString)
+    {
+        $e = explode('_', $sortingString);
+        $sortingOrder = $e[count($e)-1];
+        $sortingPath  = str_replace('_'.$sortingOrder, '', $sortingString);
+
+        return $sortingParameter = [
+            "sortingOrder" => $sortingOrder,
+            "sortingPath"  => $sortingPath
+        ];
+    }
+
     public static function buildNameSorting($path)
     {
         return str_replace('texts.', '', $path);
--- src/Services/ItemLoader/Loaders/CategoryItems.php
+++ src/Services/ItemLoader/Loaders/CategoryItems.php
@@ -148,10 +148,19 @@ class CategoryItems implements ItemLoaderContract, ItemLoaderPaginationContract,
 	public function getSorting($options = [])
     {
         $sortingInterface = null;
-        
+
         if(isset($options['sorting']) && strlen($options['sorting']))
         {
-            $sortingInterface = SortingBuilder::buildSorting($options['sorting']);
+
+            if($options['sorting'] == 'default.standard_sorting')
+            {
+                $sortingInterface = SortingBuilder::buildDefaultSorting();
+            }
+            else
+            {
+                $sortingInterface = SortingBuilder::buildSorting($options['sorting']);
+            }
+
         }
        
         return $sortingInterface;
