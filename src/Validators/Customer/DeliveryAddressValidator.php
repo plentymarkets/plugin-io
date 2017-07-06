aa8e9259a355529ce5ff315506d47e517018e261
Manuel Dierkes <manuel.dierkes@plentymarkets.com>
2017-07-06 10:42:46 +0200

    feature_optionType_Title: - added Title to Validators

--- src/Validators/Customer/DeliveryAddressValidator.php
+++ src/Validators/Customer/DeliveryAddressValidator.php
@@ -39,6 +39,7 @@ class DeliveryAddressValidator extends Validator
             $this->addString('address3',  $this->isRequired('address3'));
             $this->addString('address4',  $this->isRequired('address4'));
             $this->addString('stateId',  $this->isRequired('stateId'));
+            $this->addString('title',     $this->isRequired('title'));
         }
     }
     
