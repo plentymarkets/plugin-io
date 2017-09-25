de84bfa742f958ef7873df7c8915809a93dc965d
cjohannsen <christoph.johannsen@plentymarkets.com>
2017-09-25 13:33:37 +0200

    CHANGE OrderReturnController - when user is not the owner of the order, 404 page will open. If user isn't logged in, user will be forwarded to 404 page.

--- src/Controllers/OrderReturnController.php
+++ src/Controllers/OrderReturnController.php
@@ -3,7 +3,9 @@ namespace IO\Controllers;
 
 use IO\Helper\TemplateContainer;
 use Plenty\Plugin\ConfigRepository;
+use IO\Services\CustomerService;
 use IO\Services\OrderService;
+use IO\Guards\AuthGuard;
 
 /**
  * Class OrderReturnController
@@ -15,8 +17,13 @@ class OrderReturnController extends LayoutController
      * Render the order returns view
      * @return string
      */
-    public function showOrderReturn($orderId):string
+    public function showOrderReturn($orderId, CustomerService $customerService):string
     {
+        if($customerService->getContactId() <= 0)
+        {
+            AuthGuard::redirect("/login", ["backlink" => AuthGuard::getUrl()]);
+        }
+
         $configRepo = pluginApp(ConfigRepository::class);
         $orderService = pluginApp(OrderService::class);
         $orderData = [];
@@ -24,11 +31,21 @@ class OrderReturnController extends LayoutController
         $enabledRoutes = explode(", ",  $configRepo->get("IO.routing.enabled_routes") );
         if( in_array('order-return', $enabledRoutes) || in_array("all", $enabledRoutes))
         {
-            $orderData = $orderService->findOrderById($orderId);
+            try
+            {
+                $orderData = $orderService->findOrderById($orderId);
+                $template = 'tpl.order.return';
+            }
+            catch (\Exception $e)
+            {
+                $orderData = [];
+                $template = 'tpl.page-not-found';
+            }
+            
         }
         
         return $this->renderTemplate(
-            "tpl.order.return",
+            $template,
             ['orderData' => $orderData]
 		);
     }
