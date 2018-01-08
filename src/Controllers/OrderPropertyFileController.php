6ec3320a091f06f7a72e8cd10700b5cd16f69a3a
Dominik Meyer <dominik.meyer@plentymarkets.com>
2018-01-08 16:25:44 +0100

    FIX order property file download

--- src/Controllers/OrderPropertyFileController.php
+++ src/Controllers/OrderPropertyFileController.php
@@ -7,17 +7,47 @@ use Plenty\Plugin\Http\Response;
 
 class OrderPropertyFileController extends LayoutController
 {
-    public function showFile(string $hash, string $filename)
+    public function showFile1(string $hash1, string $filename)
     {
-        if(strlen($hash) && strlen($filename))
+        if(strlen($hash1) && strlen($filename))
         {
             /** @var OrderPropertyFileService $orderPropertyFileService */
             $orderPropertyFileService = pluginApp(OrderPropertyFileService::class);
-            $url = $orderPropertyFileService->getFileURL($hash . '/' . $filename);
+            
+            $key = $hash1.'/'.$filename;
+            
+            $url = $orderPropertyFileService->getFileURL($key);
             
             return pluginApp(Response::class)->redirectTo($url);
         }
     
+        return $this->renderTemplate(
+            "tpl.page-not-found",
+            [
+                "data" => ''
+            ]
+        );
+    }
+    
+    public function showFile2(string $hash1, string $hash2 = '', string $filename)
+    {
+        if(strlen($hash1) && strlen($filename))
+        {
+            /** @var OrderPropertyFileService $orderPropertyFileService */
+            $orderPropertyFileService = pluginApp(OrderPropertyFileService::class);
+            
+            $key = $hash1.'/';
+            if(strlen($hash2))
+            {
+                $key .= $hash2.'/';
+            }
+            $key .= $filename;
+            
+            $url = $orderPropertyFileService->getFileURL($key);
+            
+            return pluginApp(Response::class)->redirectTo($url);
+        }
+        
         return $this->renderTemplate(
             "tpl.page-not-found",
             [
