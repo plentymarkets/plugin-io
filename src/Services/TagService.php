97551b82b2fe03ade542a1227752e15585532a09
Felix Dausch <felix.dausch@plentymarkets.com>
2018-05-02 13:59:18 +0200

    ADD method to get name of a tag for a specific language

--- src/Services/TagService.php
+++ src/Services/TagService.php
@@ -4,6 +4,7 @@ namespace IO\Services;
 
 use Plenty\Modules\Authorization\Services\AuthHelper;
 use Plenty\Modules\Tag\Contracts\TagRepositoryContract;
+use Plenty\Modules\Tag\Models\Tag;
 
 
 /**
@@ -24,6 +25,12 @@ class TagService
         $this->tagRepository = $tagRepository;
     }
 
+    /**
+     * Get a tag by its id
+     *
+     * @param int $tagId    The id of the tag
+     * @return Tag
+     */
     public function getTagById(int $tagId)
     {
         /** @var AuthHelper $authHelper */
@@ -36,5 +43,35 @@ class TagService
         });
 
         return $tagData;
+    }
+
+    /**
+     * Get the name of a tag for a specific language
+     *
+     * @param int       $tagId  The id of the tag
+     * @param string    $lang   The language to get the name in.
+     * @return string
+     */
+    public function getTagName(int $tagId, $lang = null)
+    {
+        if ( $lang === null )
+        {
+            $lang = pluginApp(SessionStorageService::class)->getLang();
+        }
+
+        $tag = $this->getTagById($tagId);
+
+        if ( !is_null($tag) )
+        {
+            foreach( $tag->names as $tagName )
+            {
+                if ( $tagName->tagLang === $lang )
+                {
+                    return $tagName->tagName;
+                }
+            }
+        }
+
+        return $tag->tagName;
     }
 }
