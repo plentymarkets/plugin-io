9f9807fa6eca7c2b7331994ad7c76b5f9d22d254
cjohannsen <christoph.johannsen@plentymarkets.com>
2018-05-02 11:54:32 +0200

    ADD TagService - a new service to return the data of a tag by id

---
+++ src/Services/TagService.php
@@ -0,0 +1,42 @@
+<?php //strict
+
+namespace IO\Services;
+
+use Plenty\Modules\Authorization\Services\AuthHelper;
+use Plenty\Modules\Tag\Contracts\TagRepositoryContract;
+
+
+/**
+ * Class CustomerService
+ * @package IO\Services
+ */
+class TagService
+{
+    /**
+     * @var TagRepositoryContract
+     */
+    private $tagRepository;
+
+    /**
+     * TagService constructor.
+     * @param TagRepositoryContract $tagRepository
+     */
+    public function __construct(TagRepositoryContract $tagRepository)
+    {
+        $this->tagRepository = $tagRepository;
+    }
+
+    public function getTagById(int $tagId)
+    {
+        /** @var AuthHelper $authHelper */
+        $authHelper = pluginApp(AuthHelper::class);
+        $tagRepository = $this->tagRepository;
+
+        $tagData = $authHelper->processUnguarded( function() use ($tagRepository, $tagId)
+        {
+            return $tagRepository->getTagById($tagId);
+        });
+
+        return $tagData;
+    }
+}
