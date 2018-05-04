3fd9df7d5a4238341ab299132b3853ed7f7aa775
Felix Dausch <felix.dausch@plentymarkets.com>
2018-05-04 16:34:52 +0200

    Return default items for item lists if not category or tag id is given

--- src/Services/ItemListService.php
+++ src/Services/ItemListService.php
@@ -20,32 +20,41 @@ class ItemListService
         $searchService = pluginApp( ItemSearchService::class );
         $searchFactory = null;
 
-        switch ($type)
+        if ( ( is_null($id) || strlen($id) <= 0 ) && $type !== self::TYPE_LAST_SEEN )
         {
-            case self::TYPE_CATEGORY:
-                $searchFactory = CategoryItems::getSearchFactory([
-                    'categoryId' => $id,
-                    'sorting'    => $sorting
-                ]);
-                break;
-            case self::TYPE_LAST_SEEN:
-                /** @var SessionStorageService $sessionStorage */
-                $sessionStorage = pluginApp(SessionStorageService::class);
-                $variationIds = $sessionStorage->getSessionValue(SessionStorageKeys::LAST_SEEN_ITEMS);
+            $searchFactory = VariationList::getSearchFactory([
+                'sorting' => $sorting
+            ]);
+        }
+        else
+        {
+            switch ($type)
+            {
+                case self::TYPE_CATEGORY:
+                    $searchFactory = CategoryItems::getSearchFactory([
+                        'categoryId' => $id,
+                        'sorting'    => $sorting
+                    ]);
+                    break;
+                case self::TYPE_LAST_SEEN:
+                    /** @var SessionStorageService $sessionStorage */
+                    $sessionStorage = pluginApp(SessionStorageService::class);
+                    $variationIds = $sessionStorage->getSessionValue(SessionStorageKeys::LAST_SEEN_ITEMS);
 
-                $searchFactory = VariationList::getSearchFactory([
-                    'variationIds'  => $variationIds,
-                    'sorting'       => $sorting
-                ]);
-                break;
-            case self::TYPE_TAG:
-                $searchFactory = TagItems::getSearchFactory([
-                    'tagIds'    => [$id],
-                    'sorting'   => $sorting
-                ]);
-                break;
-            default:
-                break;
+                    $searchFactory = VariationList::getSearchFactory([
+                        'variationIds'  => $variationIds,
+                        'sorting'       => $sorting
+                    ]);
+                    break;
+                case self::TYPE_TAG:
+                    $searchFactory = TagItems::getSearchFactory([
+                        'tagIds'    => [$id],
+                        'sorting'   => $sorting
+                    ]);
+                    break;
+                default:
+                    break;
+            }
         }
 
         if ( is_null($searchFactory) )
