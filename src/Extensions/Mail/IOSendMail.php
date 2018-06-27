174c068307c0df6f41916a7f92816d13cb4fdbc5
Steve Tentrop <steve.tentrop@plentymarkets.com>
2018-06-27 10:55:13 +0200

    handle send mail event

---
+++ src/Extensions/Mail/IOSendMail.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace IO\Extensions\Mail;
+
+use IO\Services\CustomerPasswordResetService;
+use Plenty\Modules\Plugin\Events\PluginSendMail;
+use Plenty\Modules\Plugin\Services\PluginSendMailService;
+
+
+class IOSendMail
+{
+
+    /**
+     * @param PluginSendMail $pluginSendMail
+     */
+    public function handle(PluginSendMail $pluginSendMail)
+    {
+        $template = $pluginSendMail->getTemplate();
+        $email = $pluginSendMail->getContactEmail();
+
+
+        /** @var PluginSendMailService $pluginSendMailService */
+        $pluginSendMailService = pluginApp(PluginSendMailService::class);
+
+        $response = null;
+        if(strlen($email) > 0)
+        {
+            switch($template)
+            {
+                case 'reset_pw':
+                    /**
+                     * @var CustomerPasswordResetService $customerPasswordResetService
+                     */
+                    $customerPasswordResetService = pluginApp(CustomerPasswordResetService::class);
+                    $response = $customerPasswordResetService->resetPassword($email, 'Ceres::Customer.ResetPasswordMail', 'Ceres::Template.resetPwMailSubject');
+                    break;
+            }
+        }
+
+        if($response === true)
+        {
+            $pluginSendMailService->setStatus(true);
+        }
+        else
+        {
+            $pluginSendMailService->setStatus(false);
+        }
+    }
+}
