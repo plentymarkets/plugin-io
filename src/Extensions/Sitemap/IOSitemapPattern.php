561a6b1b01ce30f996ae97e33b7006724b233963
Steve Tentrop <steve.tentrop@plentymarkets.com>
2018-07-03 11:36:04 +0200

    remove check

--- src/Extensions/Sitemap/IOSitemapPattern.php
+++ src/Extensions/Sitemap/IOSitemapPattern.php
@@ -3,7 +3,6 @@
 namespace IO\Extensions\Sitemap;
 
 use IO\Services\TemplateConfigService;
-use Plenty\Modules\Plugin\Contracts\PluginRepositoryContract;
 use Plenty\Modules\Plugin\Events\LoadSitemapPattern;
 use Plenty\Modules\Plugin\Models\Plugin;
 use Plenty\Modules\Plugin\Services\PluginSeoSitemapService;
@@ -25,51 +24,42 @@ class IOSitemapPattern
      */
     public function handle(LoadSitemapPattern $sitemapPattern)
     {
-        /** @var PluginRepositoryContract $pluginRepository */
-        $pluginRepository = pluginApp(PluginRepositoryContract::class);
-        
-        $pluginList = $pluginRepository->searchPlugins(['name' => 'IO'])->getResult();
-        $plugin = $pluginList[0];
-        
-        if($plugin instanceof Plugin && $plugin->activeProductive)
+        /** @var PluginSeoSitemapService $seoSitemapService */
+        $seoSitemapService = pluginApp(PluginSeoSitemapService::class);
+
+        /** @var TemplateConfigService $templateConfigService */
+        $templateConfigService = pluginApp(TemplateConfigService::class);
+        $enableOldURLPattern = $templateConfigService->get('global.enableOldUrlPattern');
+
+        if(!strlen($enableOldURLPattern) || $enableOldURLPattern == 'false')
         {
-            /** @var PluginSeoSitemapService $seoSitemapService */
-            $seoSitemapService = pluginApp(PluginSeoSitemapService::class);
-    
-            /** @var TemplateConfigService $templateConfigService */
-            $templateConfigService = pluginApp(TemplateConfigService::class);
-            $enableOldURLPattern = $templateConfigService->get('global.enableOldUrlPattern');
-    
-            if(!strlen($enableOldURLPattern) || $enableOldURLPattern == 'false')
+            $itemPattern = [
+                'pattern' => '_{itemId}_{variationId?}',
+                'container' => ''
+            ];
+
+            $seoSitemapService->setItemPattern($itemPattern);
+        }
+
+        /** @var ConfigRepository $configRepository */
+        $configRepository = pluginApp(ConfigRepository::class);
+
+        $contentRoutes = [];
+        $enabledRoutes = explode(', ', $configRepository->get('IO.routing.enabled_routes', ''));
+        if(count($enabledRoutes))
+        {
+            foreach($this->contentRoutes as $route)
             {
-                $itemPattern = [
-                    'pattern' => '_{itemId}_{variationId?}',
-                    'container' => ''
-                ];
-        
-                $seoSitemapService->setItemPattern($itemPattern);
-            }
-    
-            /** @var ConfigRepository $configRepository */
-            $configRepository = pluginApp(ConfigRepository::class);
-            
-            $contentRoutes = [];
-            $enabledRoutes = explode(', ', $configRepository->get('IO.routing.enabled_routes', ''));
-            if(count($enabledRoutes))
-            {
-                foreach($this->contentRoutes as $route)
+                if(in_array($route, $enabledRoutes))
                 {
-                    if(in_array($route, $enabledRoutes))
-                    {
-                        $contentRoutes[] = ['url' => $route];
-                    }
+                    $contentRoutes[] = ['url' => $route];
                 }
             }
-    
-            if(count($contentRoutes))
-            {
-                $seoSitemapService->setContentCategoryPattern(['pattern' => '', 'container' => $contentRoutes]);
-            }
+        }
+
+        if(count($contentRoutes))
+        {
+            $seoSitemapService->setContentCategoryPattern(['pattern' => '', 'container' => $contentRoutes]);
         }
     }
 }
