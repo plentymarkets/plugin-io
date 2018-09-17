9e3308ab31b8da0df5eb03500e6f5aa3f8dd0e9a
Martijn Klaes <martijn.klaes@plentymarkets.com>
2018-09-17 10:52:41 +0200

    plugin-test-suite compat

---
+++ tests/TestCase.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Plenty\Modules\Plugin\Tests;
+
+use Tests\BrowserKitTestCase;
+
+/**
+ * Class TestCase
+ */
+class TestCase extends BrowserKitTestCase
+{
+    protected function setUp()
+	{
+		parent::setUp();
+	}
+}
