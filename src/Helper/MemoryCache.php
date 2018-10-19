cd1a5f7091d97a43569da8a6679cfc8bfde1a577
Manuel Dierkes <manuel.dierkes@plentymarkets.com>
2018-10-19 11:56:37 +0200

    added checkoutTests

--- src/Helper/MemoryCache.php
+++ src/Helper/MemoryCache.php
@@ -8,6 +8,18 @@ trait MemoryCache
 
     protected function fromMemoryCache( $key, \Closure $callack )
     {
+        /*
+
+         TODO:
+         Make this dirty workaround obsolete by implementing a memory cache without using static stuff.
+         Use the service container and register the class as singleton. Maybe develop a memory cache in
+         the core codebase and make it available in the interface.
+
+        */
+        if (getenv('APP_ENV') === 'testing') {
+            return $callack->call($this);
+        }
+
         if (!array_key_exists(self::class, self::$cache))
         {
             self::$cache[self::class] = [];
@@ -20,4 +32,5 @@ trait MemoryCache
 
         return self::$cache[self::class][$key];
     }
+
 }
