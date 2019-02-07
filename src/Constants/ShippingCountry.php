5f020d8c3a723673184c5a7179d13058b85b4e47
Felix Dausch <felix.dausch@plentymarkets.com>
2019-02-07 14:48:21 +0100

    Remove customizable address validators

--- src/Constants/ShippingCountry.php
+++ src/Constants/ShippingCountry.php
@@ -33,4 +33,19 @@ class ShippingCountry
     const POLAND = 23;
     const SWEDEN = 24;
     const SINGAPORE = 25;
+
+    const ADDRESS_FORMAT_DE = 'DE';
+    const ADDRESS_FORMAT_EN = 'EN';
+
+    public static function getAddressFormat($countryId)
+    {
+        switch($countryId)
+        {
+            case self::UNITED_KINGDOM:
+            case self::IRELAND:
+                return self::ADDRESS_FORMAT_EN;
+        }
+
+        return self::ADDRESS_FORMAT_DE;
+    }
 }
