07ab0e3b8288c8a1221056b5f54328381023780f
Dominik Meyer <dominik.meyer@plentymarkets.com>
2019-02-28 17:38:57 +0100

    FIX order document route

--- src/Providers/IORouteServiceProvider.php
+++ src/Providers/IORouteServiceProvider.php
@@ -212,7 +212,7 @@ class IORouteServiceProvider extends RouteServiceProvider
         
         if( RouteConfig::isActive(RouteConfig::ORDER_DOCUMENT) )
         {
-            $router->get('order-document/{documentId}', 'IO\Controllers\DocumentController@download');
+            $router->get('order-document/preview/{documentId}', 'IO\Controllers\DocumentController@preview');
         }
 
         
