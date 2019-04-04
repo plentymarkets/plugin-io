fa85df50669534ad723b775d26751e04f9061298
Felix Dausch <felix.dausch@plentymarkets.com>
2019-04-04 13:56:59 +0200

    ADD logs for category controller

--- src/Controllers/CategoryController.php
+++ src/Controllers/CategoryController.php
@@ -10,6 +10,7 @@ use Plenty\Modules\ShopBuilder\Helper\ShopBuilderRequest;
 use Plenty\Plugin\Application;
 use Plenty\Plugin\Http\Request;
 use Plenty\Plugin\Http\Response;
+use Plenty\Plugin\Log\Loggable;
 
 /**
  * Class CategoryController
@@ -17,6 +18,8 @@ use Plenty\Plugin\Http\Response;
  */
 class CategoryController extends LayoutController
 {
+    use Loggable;
+
     /**
      * Prepare and render the data for categories
      * @param string $lvl1 Level 1 of category url. Will be null at root page
@@ -45,9 +48,19 @@ class CategoryController extends LayoutController
 
         $category = $this->categoryRepo->findCategoryByUrl($lvl1, $lvl2, $lvl3, $lvl4, $lvl5, $lvl6, $webstoreId, $lang);
 
-
         if ($category === null || (($category->clients->count() == 0 || $category->details->count() == 0) && !$this->app->isAdminPreview()))
         {
+            $this->getLogger(__CLASS__)->warning(
+                "IO::Debug.CategoryController_cannotDisplayCategory",
+                [
+                    "branch" => [$lvl1, $lvl2, $lvl3, $lvl4, $lvl5, $lvl6],
+                    "category" => $category,
+                    "clientCount" => ($category !== null ? $category->clients->count() : 0),
+                    "detailCount" => ($category !== null ? $category->details->count() : 0),
+                    "isAdminPreview" => $this->app->isAdminPreview()
+                ]
+            );
+
             /** @var Response $response */
             $response = pluginApp(Response::class);
             $response->forceStatus(ResponseCode::NOT_FOUND);
@@ -66,6 +79,13 @@ class CategoryController extends LayoutController
 
         if ( RouteConfig::getCategoryId( RouteConfig::CHECKOUT ) === $category->id || $shopBuilderRequest->getPreviewContentType() === 'checkout')
         {
+            $this->getLogger(__CLASS__)->info(
+                "IO::Debug.CategoryController_showCheckoutCategory",
+                [
+                    "category" => $category,
+                    "previewContentType" => $shopBuilderRequest->getPreviewContentType()
+                ]
+            );
             RouteConfig::overrideCategoryId(RouteConfig::CHECKOUT, $category->id);
 
             /** @var CheckoutController $checkoutController */
@@ -75,6 +95,13 @@ class CategoryController extends LayoutController
 
         if ( RouteConfig::getCategoryId( RouteConfig::MY_ACCOUNT ) === $category->id || $shopBuilderRequest->getPreviewContentType() === 'myaccount')
         {
+            $this->getLogger(__CLASS__)->info(
+                "IO::Debug.CategoryController_showMyAccountCategory",
+                [
+                    "category" => $category,
+                    "previewContentType" => $shopBuilderRequest->getPreviewContentType()
+                ]
+            );
             RouteConfig::overrideCategoryId(RouteConfig::MY_ACCOUNT, $category->id);
 
             /** @var MyAccountController $myAccountController */
