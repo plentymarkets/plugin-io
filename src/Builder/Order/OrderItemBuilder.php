da1acfe3693baeb06b8f2ba2d961ee82a44c827c
Steve Tentrop <steve.tentrop@plentymarkets.com>
2019-05-13 14:40:20 +0200

    use vat field from variation

--- src/Builder/Order/OrderItemBuilder.php
+++ src/Builder/Order/OrderItemBuilder.php
@@ -276,7 +276,7 @@ class OrderItemBuilder
 			"shippingProfileId" => $basketItem['shippingProfileId'],
 			"countryVatId"      => $this->vatService->getCountryVatId(),
 			"vatRate"           => $basketItem['vat'],
-            "vatField"			=> $this->getVatField($this->vatService->getVat(), $basketItem['vat']),
+            "vatField"			=> $basketItem['variation']['data']['variation']['vatId'],
             "orderProperties"   => $basketItemProperties,
             "properties"        => $properties,
 			"amounts"           => [
