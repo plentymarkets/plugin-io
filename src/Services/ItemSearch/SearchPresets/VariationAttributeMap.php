c080812f822cbf34c28c361a3276bdcea179673a
cjohannsen <christoph.johannsen@plentymarkets.com>
2019-07-12 15:01:27 +0200

    CHANGE VariationAttributeMap - remove isSalable

--- src/Services/ItemSearch/SearchPresets/VariationAttributeMap.php
+++ src/Services/ItemSearch/SearchPresets/VariationAttributeMap.php
@@ -33,12 +33,6 @@ class VariationAttributeMap implements SearchPreset
             ->withReducedResults()
             ->withLinkToContent();
 
-        if(pluginApp(TemplateConfigService::class)->get('item.show_variation_over_dropdown') != 'true')
-        {
-            $searchFactory
-                ->isSalable();
-        }
-        
         if(array_key_exists('itemId', $options) && $options['itemId'] != 0)
         {
             $searchFactory->hasItemId($options['itemId']);
