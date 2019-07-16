14afcaf24de9eebb005eaa61b45bc56e0a21f0de
Dominik Meyer <dominik.meyer@plentymarkets.com>
2019-07-16 11:45:19 +0200

    FIX address validation

--- src/Validators/Customer/AddressValidator.php
+++ src/Validators/Customer/AddressValidator.php
@@ -16,9 +16,9 @@ class AddressValidator extends Validator
         $this->addString('address2', $addressFormat === ShippingCountry::ADDRESS_FORMAT_DE);
         $this->addString('postalCode', true);
         $this->addString('town', true);
-        $this->addString('name1', $isCompany);
-        $this->addString('name2', !$isCompany);
-        $this->addString('name3', !$isCompany);
+        $this->addString('name1', $isCompany)->nullable();
+        $this->addString('name2', !$isCompany)->nullable();
+        $this->addString('name3', !$isCompany)->nullable();
         $this->addDate('birthday')->date()->dateBefore(date('Y-m-d'));
     }
 }
