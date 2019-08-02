9e4254461c22ad1868406227ff5c68ce2a3bbaf3
Felix Gehrmann <felix.gehrmann@plentymarkets.com>
2019-08-02 18:26:58 +0200

    Follow up on the bug, where another case was fixed

--- src/Services/CouponService.php
+++ src/Services/CouponService.php
@@ -2,6 +2,7 @@
 
 namespace IO\Services;
 
+use Illuminate\Database\Eloquent\Collection;
 use Plenty\Modules\Authorization\Services\AuthHelper;
 use Plenty\Modules\Basket\Contracts\BasketRepositoryContract;
 use Plenty\Modules\Basket\Models\Basket;
@@ -90,6 +91,7 @@ class CouponService
     }
 
     /**
+     * Remove a coupon and throw a notification
      * @param int $code Infocode for notification
      */
     private function removeInvalidCoupon($code)
@@ -100,13 +102,33 @@ class CouponService
 
     private function isCouponMinimalOrderOnUpdate($basket, $data, $basketItem, $campaign): bool
     {
-        // $basket->basketAmount is basket amount minus coupon value
-        // $basket->couponDiscount is negative
-        $minimalOrder = $campaign->minOrderValue;
+        // Early exit if the quantity was raised ( careful: quantityChange is negative when raised )
         $quantityChange = $basketItem['quantity'] - $data['quantity'];
-        $newBasketAmount = (( $basket->basketAmount - $basket->couponDiscount ) - ($basketItem['price'] * $quantityChange));
+        if($quantityChange <= 0) {
+            return false;
+        }
 
-        return $minimalOrder > $newBasketAmount;
+        // Get the normal basket items (not associated with the coupon)
+        $normalBasketItems = $this->getNormalBasketItems($basket, $campaign);
+        // Early exit if itemChanged ($data) is a normal item
+        foreach($normalBasketItems as $item) {
+            if($item->itemId == $data['itemId']) {
+                return false;
+            }
+        }
+
+        // Remove couponDiscount and normal items from basketAmount
+        $basketAmountWithoutCoupon = $basket->basketAmount - $basket->couponDiscount;
+        $basketAmountWithoutNormal = $basketAmountWithoutCoupon;
+
+        foreach($normalBasketItems as $item) {
+            $basketAmountWithoutNormal -= $item->price * $item->quantity;
+        }
+
+        // Subtract the changed quantity from the basketAmountWithoutNormal
+        $basketAmountFinal = $basketAmountWithoutNormal - ($data['price'] * $quantityChange);
+
+        return $campaign->minOrderValue > $basketAmountFinal;
     }
 
     private function isCouponMinimalOrderOnDelete($basket, $basketItem, $campaign): bool
@@ -125,23 +147,28 @@ class CouponService
          */
         $variationCategoryRepository = pluginApp(VariationCategoryRepositoryContract::class);
 
+        // Get the categories of this variation
         $authHelper = pluginApp(AuthHelper::class);
         $variationId = $basketItem['variationId'];
         $categories = $authHelper->processUnguarded(function () use ($variationId, $variationCategoryRepository) {
             return $variationCategoryRepository->findByVariationIdWithInheritance($variationId);
         });
 
+        // Transform categories in an array of category ids
         $categoryIds = [];
         $categories->each(function ($category) use (&$categoryIds) {
             $categoryIds[] = $category->categoryId;
         });
 
+        // Get items associated with the campaign via categoryid
         $campaignItems = $campaign->references->where('referenceType', 'category')->whereIn('value', $categoryIds);
         if (count($campaignItems) == 0) {
+            // In case no category was linked, use item refs directly
             $campaignItems = $campaign->references->where('referenceType', 'item')->where('value',
                 $basketItem['itemId']);
         }
 
+        // If this block is not entered, the coupon has no items associated
         if (count($campaignItems)) {
             $matchingBasketItems = $basket->basketItems->where('itemId', $basketItem['itemId']);
 
@@ -183,5 +210,57 @@ class CouponService
         }
 
         return false;
+    }
+
+    /**
+     * Get all items in basket, which are associated with the current campaign
+     * @param $basket
+     * @param $campaign
+     * @return array
+     */
+    private function getNormalBasketItems($basket, $campaign)
+    {
+        /**
+         * @var Collection $basketItems
+         */
+        $basketItems = $basket->basketItems;
+        $campaignBasketItems = [];
+
+        $variationCategoryRepository = pluginApp(VariationCategoryRepositoryContract::class);
+        $authHelper = pluginApp(AuthHelper::class);
+
+        $basketItems->each(function ($item) use (
+            &$campaignBasketItems,
+            $campaign,
+            $authHelper,
+            $variationCategoryRepository
+        )
+        {
+            $variationId = $item->variationId;
+            $categories = $authHelper->processUnguarded(function () use (
+                $variationId,
+                $variationCategoryRepository
+            )
+            {
+                return $variationCategoryRepository->findByVariationIdWithInheritance($variationId);
+            });
+
+            $categoryIds = [];
+            $categories->each(function ($category) use (&$categoryIds) {
+                $categoryIds[] = $category->categoryId;
+            });
+
+            $campaignItems = $campaign->references->where('referenceType', 'category')->whereIn('value',
+                $categoryIds);
+            if (count($campaignItems) === 0) {
+                $campaignItems = $campaign->references->where('referenceType', 'item')->where('value',
+                    $item->itemId);
+            }
+            if (count($campaignItems) === 0) {
+                $campaignBasketItems[] = $item;
+            }
+        });
+
+        return $campaignBasketItems;
     }
 }
