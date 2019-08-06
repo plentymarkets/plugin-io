611b1e014e8c69e9e0c33e467f5a5b0cecad4484
Lukas Matzen <lks.mtzn@gmail.com>
2019-08-06 16:21:28 +0200

    UPDATE not enough stock exception with stock param

--- src/Api/Resources/BasketItemResource.php
+++ src/Api/Resources/BasketItemResource.php
@@ -54,7 +54,7 @@ class BasketItemResource extends ApiResource
 
         if(array_key_exists("code", $basketItems))
         {
-            return $this->response->create(["exceptionCode" => $basketItems["code"]], ResponseCode::BAD_REQUEST);
+            return $this->response->create(["exceptionCode" => $basketItems["code"], 'placeholder' => $basketItems['placeholder']], ResponseCode::BAD_REQUEST);
         }
 
 		return $this->response->create($basketItems, ResponseCode::CREATED);
