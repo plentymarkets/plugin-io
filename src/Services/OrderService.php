fe0829e867eeb5523bf79debc7fd2fc8738ed1de
Felix Dausch <felix.dausch@plentymarkets.com>
2019-08-21 18:04:01 +0200

    Remove temporary method

--- src/Services/OrderService.php
+++ src/Services/OrderService.php
@@ -776,15 +776,4 @@ class OrderService
 
         return $paymentProperty;
     }
-    
-    public function getRandomOrder()
-    {
-        $filters['orderType'] = OrderType::ORDER;
-        $this->orderRepository->setFilters($filters);
-        $orderList = $this->orderRepository->searchOrders(1, 1)->getResult();
-        $orderArray = $orderList->first();
-        $order = $this->findOrderById($orderArray['id']);
-        
-        return $order;
-    }
 }
