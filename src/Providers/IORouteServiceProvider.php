f604da7ed6069bdf31cd29dbfabae4438d572e4b
Felix Gehrmann <felix.gehrmann@plentymarkets.com>
2019-09-17 18:44:02 +0200

    Add route for fakerbasket

--- src/Providers/IORouteServiceProvider.php
+++ src/Providers/IORouteServiceProvider.php
@@ -76,7 +76,7 @@ class IORouteServiceProvider extends RouteServiceProvider
             $api->resource('io/live-shopping', 'LiveShoppingResource');
             $api->resource('io/facet', 'FacetResource');
             $api->resource('io/categorytree', 'CategoryTreeResource');
-
+            $api->resource('io/faker/basket', 'FakerBasketResource');
 		});
 
 		/** @var ShopUrls $shopUrls */
