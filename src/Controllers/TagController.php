7e3ec06aa33002ebbf2c2469628861abee382825
Maximilian Röll <maximilian.roell@plentymarkets.com>
2019-11-12 16:41:07 +0100

    # fixed typo

--- src/Controllers/TagController.php
+++ src/Controllers/TagController.php
@@ -14,12 +14,12 @@ class TagController extends LayoutController
      * @param string $name
      * @param int $TagId
      */
-    public function showItemByTag(string $tagName = "", int $TagId = null)
+    public function showItemByTag(string $tagName = "", int $tagId = null)
     {
         return $this->renderTemplate(
             'tpl.tags',
             [
-                "tagId" => $TagId,
+                "tagId" => $tagId,
                 "tagName" => $tagName
             ]
         );
