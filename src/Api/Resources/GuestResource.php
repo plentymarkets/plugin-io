40ee929d57080e3a6749f431c4770ca57bdb2e37
Dominik Meyer <dominik.meyer@plentymarkets.com>
2019-12-06 15:51:52 +0100

    FIX guest login

--- src/Api/Resources/GuestResource.php
+++ src/Api/Resources/GuestResource.php
@@ -2,6 +2,8 @@
 
 namespace IO\Api\Resources;
 
+use IO\Builder\Order\AddressType;
+use IO\Services\CustomerService;
 use Plenty\Plugin\Http\Response;
 use Plenty\Plugin\Http\Request;
 use IO\Api\ApiResource;
@@ -13,21 +15,47 @@ use IO\Constants\SessionStorageKeys;
 
 class GuestResource extends ApiResource
 {
-    public function __construct(Request $request, ApiResponse $response)
+    /** @var CustomerService */
+    private $customerService;
+    
+    public function __construct(Request $request, ApiResponse $response, CustomerService $customerService)
     {
         parent::__construct($request, $response);
+        $this->customerService = $customerService;
     }
     
-    public function store():Response
+    public function store(): Response
     {
         $email = $this->request->get('email', '');
-    
+        
         /**
          * @var SessionStorageService $sessionStorage
          */
         $sessionStorage = pluginApp(SessionStorageService::class);
+        
+        $existingEmail = $sessionStorage->getSessionValue(SessionStorageKeys::GUEST_EMAIL);
+        if (!is_null($existingEmail) && strlen($existingEmail) && $email !== $existingEmail) {
+            $addressList[AddressType::BILLING]  = $this->customerService->getAddresses(AddressType::BILLING);
+            $addressList[AddressType::DELIVERY] = $this->customerService->getAddresses(AddressType::DELIVERY);
+            
+            if (count($addressList)) {
+                foreach ($addressList as $type => $addresses) {
+                    if (count($addresses)) {
+                        $this->deleteAddresses($type, $addresses);
+                    }
+                }
+            }
+        }
+        
         $sessionStorage->setSessionValue(SessionStorageKeys::GUEST_EMAIL, $email);
         
         return $this->response->create($email, ResponseCode::OK);
+    }
+    
+    private function deleteAddresses($type, $addresses)
+    {
+        foreach ($addresses as $address) {
+            $this->customerService->deleteAddress($address->id, $type);
+        }
     }
 }
