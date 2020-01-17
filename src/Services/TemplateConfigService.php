87d7ddb65775f329b49896cc77eeb0f5750d78a6
Dominik Meyer <dominik.meyer@plentymarkets.com>
2020-01-17 10:55:30 +0100

    UPDATE deprecate TemplateConfigService

--- src/Services/TemplateConfigService.php
+++ src/Services/TemplateConfigService.php
@@ -2,43 +2,64 @@
 
 namespace IO\Services;
 
+use Plenty\Modules\Webshop\Template\Contracts\TemplateConfigRepositoryContract;
 use Plenty\Plugin\ConfigRepository;
 
+/**
+ * Class TemplateConfigService
+ * @package IO\Services
+ * @deprecated since 5.0.0 will be deleted in 6.0.0
+ * @see \Plenty\Modules\Webshop\Template\Contracts\TemplateConfigRepositoryContract
+ */
 class TemplateConfigService
 {
     private $configRepository;
     private $templatePluginName;
 
-    public function __construct(ConfigRepository $configRepository)
+    /** @var TemplateConfigRepositoryContract $templateConfigRepo */
+    private $templateConfigRepo;
+
+    public function __construct(ConfigRepository $configRepository, TemplateConfigRepositoryContract $templateConfigRepo)
     {
         $this->configRepository = $configRepository;
         $this->templatePluginName = $this->configRepository->get('IO.template.template_plugin_name');
+
+        $this->templateConfigRepo = $templateConfigRepo;
     }
 
+    /**
+     * @param $key
+     * @param null $default
+     * @return mixed|null
+     * @deprecated since 5.0.0 will be deleted in 6.0.0
+     * @see \Plenty\Modules\Webshop\Template\Contracts\TemplateConfigRepositoryContract::get()
+     */
     public function get($key, $default = null)
     {
-        if(strlen($this->templatePluginName))
-        {
-            return $this->configRepository->get($this->templatePluginName.'.'.$key, $default);
-        }
-
-        return null;
+        return $this->templateConfigRepo->get($key, $default);
     }
 
+    /**
+     * @param $key
+     * @param bool $default
+     * @return mixed
+     * @deprecated since 5.0.0 will be deleted in 6.0.0
+     * @see \Plenty\Modules\Webshop\Template\Contracts\TemplateConfigRepositoryContract::getBoolean()
+     */
     public function getBoolean($key, $default = false)
     {
-        $value = $this->get($key);
-
-        if ( $value === "true" || $value === "false"  || $value === "1" || $value === "0" || $value === 1 || $value === 0)
-        {
-            return $value === "true" || $value === "1" || $value === 1;
-        }
-
-        return $default;
+        return $this->templateConfigRepo->getBoolean($key, $default);
     }
 
+    /**
+     * @param $key
+     * @param int $default
+     * @return mixed
+     * @deprecated since 5.0.0 will be deleted in 6.0.0
+     * @see \Plenty\Modules\Webshop\Template\Contracts\TemplateConfigRepositoryContract::getInteger()
+     */
     public function getInteger($key, $default = 0)
     {
-        return intval($this->get($key, $default));
+        return $this->templateConfigRepo->getInteger($key, $default);
     }
 }
