2c811c8eabba9cc7c8e36db090026aee9fcbb210
Felix Dausch <felix.dausch@plentymarkets.com>
2020-02-17 14:17:30 +0100

    Reset cached data after basket changed to force recalculation if net prices should be shown.

--- src/Services/CustomerService.php
+++ src/Services/CustomerService.php
@@ -79,7 +79,8 @@ class CustomerService
         ContactAddressRepositoryContract $contactAddressRepository,
         AddressRepositoryContract $addressRepository,
         ContactClassRepositoryContract $contactClassRepository,
-        SessionStorageService $sessionStorage
+        SessionStorageService $sessionStorage,
+        Dispatcher $dispatcher
     ) {
         $this->accountRepository = $accountRepository;
         $this->contactRepository = $contactRepository;
@@ -87,6 +88,10 @@ class CustomerService
         $this->addressRepository = $addressRepository;
         $this->contactClassRepository = $contactClassRepository;
         $this->sessionStorage = $sessionStorage;
+        $dispatcher->listen(AfterBasketChanged::class, function()
+        {
+            $this->resetMemoryCache();
+        });
     }
 
     /**
