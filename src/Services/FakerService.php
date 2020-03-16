d3f2eb283fe67058060616ab4eb6c5c38c53798a
Felix Dausch <felix.dausch@plentymarkets.com>
2020-03-16 16:29:31 +0100

    ADD method to fake item search results

--- src/Services/FakerService.php
+++ src/Services/FakerService.php
@@ -4,6 +4,8 @@ namespace IO\Services;
 
 use IO\Services\Basket\Factories\BasketResultFactory;
 use IO\Services\ItemSearch\Factories\Faker\FacetFaker;
+use IO\Services\ItemSearch\Factories\VariationSearchResultFactory;
+use Plenty\Modules\Webshop\ItemSearch\Helpers\ResultFieldTemplate;
 
 class FakerService
 {
@@ -16,26 +18,36 @@ class FakerService
         $basketResultFactory = pluginApp(BasketResultFactory::class);
         return $basketResultFactory->fillBasketResult();
     }
-    
+
     public function getFacets()
     {
         /** @var FacetFaker $facetFaker */
         $facetFaker = pluginApp(FacetFaker::class);
         $facetResult = $facetFaker->fill([]);
-        
+
         return [
             $facetResult[0]
         ];
     }
-    
+
     public function getSelectedFacetIds($facets)
     {
         $selectedFacetIds = [];
-        foreach($facets[0]['values'] as $facetValue)
-        {
+        foreach ($facets[0]['values'] as $facetValue) {
             $selectedFacetIds[] = $facetValue['id'];
         }
-        
+
         return $selectedFacetIds;
+    }
+
+    public function getSearchResult($resultFieldTemplate = ResultFieldTemplate::TEMPLATE_SINGLE_ITEM, $numberOfEntries = 1)
+    {
+        /** @var VariationSearchResultFactory $searchResultFactory */
+        $searchResultFactory = pluginApp(VariationSearchResultFactory::class);
+        return $searchResultFactory->fillSearchResults(
+            [],
+            ResultFieldTemplate::get(ResultFieldTemplate::TEMPLATE_SINGLE_ITEM),
+            $numberOfEntries
+        );
     }
 }
