5df8a9bde3e8deb42afd296cd71174f192e6e45a
Dominik Meyer <dominik.meyer@plentymarkets.com>
2020-05-08 19:43:08 +0200

    FIX call controller for logout on password and email change

--- src/Controllers/CustomerPasswordResetController.php
+++ src/Controllers/CustomerPasswordResetController.php
@@ -3,8 +3,10 @@ namespace IO\Controllers;
 
 use IO\Constants\LogLevel;
 use IO\Helper\RouteConfig;
+use IO\Services\AuthenticationService;
 use IO\Services\NotificationService;
 use IO\Services\UserDataHashService;
+use Plenty\Modules\ShopBuilder\Helper\ShopBuilderRequest;
 
 /**
  * Class CustomerPasswordResetController
@@ -16,50 +18,53 @@ class CustomerPasswordResetController extends LayoutController
      * Prepare and render the data for the guest registration
      * @return string
      */
-    public function showReset($contactId, $hash): string
+    public function showReset($contactId, $hash, $category = null): string
     {
+        /** @var AuthenticationService $authService */
+        $authService = pluginApp(AuthenticationService::class);
+        $authService->logout();
+
         /** @var UserDataHashService $hashService */
         $hashService = pluginApp(UserDataHashService::class);
         $hashData = $hashService->getData($hash, $contactId);
-        
-        if(!is_null($hashData))
-        {
+
+        /** @var ShopBuilderRequest $shopBuilderRequest */
+        $shopBuilderRequest = pluginApp(ShopBuilderRequest::class);
+
+
+        if (!is_null($hashData) || $shopBuilderRequest->isShopBuilder()) {
             return $this->renderTemplate(
                 "tpl.password-reset",
                 [
                     "contactId" => $contactId,
-                    "hash"      => $hash
+                    "hash" => $hash,
+                    "category" => $category
                 ],
                 false
             );
-        }
-        else
-        {
+        } else {
             /**
              * @var NotificationService $notificationService
              */
             $notificationService = pluginApp(NotificationService::class);
-            $notificationService->addNotificationCode(LogLevel::ERROR,3);
-    
+            $notificationService->addNotificationCode(LogLevel::ERROR, 3);
+
             /** @var HomepageController $homepageController */
             $homepageController = pluginApp(HomepageController::class);
-    
-            if(RouteConfig::getCategoryId(RouteConfig::HOME) > 0)
-            {
+
+            if (RouteConfig::getCategoryId(RouteConfig::HOME) > 0) {
                 return $homepageController->showHomepageCategory();
             }
-    
+
             return $homepageController->showHomepage();
         }
-        
     }
 
     public function redirect($contactId, $hash)
     {
         $passwordResetParams = [];
 
-        if((int)$contactId > 0 && strlen($hash))
-        {
+        if ((int)$contactId > 0 && strlen($hash)) {
             $passwordResetParams['contactId'] = $contactId;
             $passwordResetParams['hash'] = $hash;
         }
@@ -67,6 +72,10 @@ class CustomerPasswordResetController extends LayoutController
         /** @var CategoryController $categoryController */
         $categoryController = pluginApp(CategoryController::class);
 
-        return $categoryController->redirectToCategory(RouteConfig::getCategoryId(RouteConfig::PASSWORD_RESET), '/password-reset', $passwordResetParams);
+        return $categoryController->redirectToCategory(
+            RouteConfig::getCategoryId(RouteConfig::PASSWORD_RESET),
+            '/password-reset',
+            $passwordResetParams
+        );
     }
 }
