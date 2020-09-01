972fe98ed2518147b80dd4b2b1bf8ccccb6c6b6c
Felix Dausch <felix.dausch@plentymarkets.com>
2020-09-01 17:59:53 +0200

    ADD doc comments

--- src/Extensions/Functions/QueryString.php
+++ src/Extensions/Functions/QueryString.php
@@ -1,10 +1,17 @@
-<?php //strict
+<?php
 
 namespace IO\Extensions\Functions;
 
 use IO\Extensions\AbstractFunction;
 use Plenty\Plugin\Http\Request;
 
+/**
+ * Class QueryString
+ *
+ * Registers a global twig function to get the query string of the current request and update or add multiple parameters.
+ *
+ * @package IO\Extensions\Functions
+ */
 class QueryString extends AbstractFunction
 {
     public function construct()
@@ -12,7 +19,8 @@ class QueryString extends AbstractFunction
     }
 
     /**
-     * Return the available methods
+     * Return all available twig functions to be registered globally.
+     *
      * @return array
      */
     public function getFunctions():array
@@ -23,13 +31,18 @@ class QueryString extends AbstractFunction
     }
 
     /**
-     * @param array $params
-     * @return string
+     * Get the current query string from the URL.
+     * Passed parameters will be replaced or added to the returned query string.
+     *
+     * @param array $params An associative array of parameter keys and values to replace or add to the query string.
+     *
+     * @return string A query string containing all query parameters of the current request and all defined parameters. Already includes a leading '?'.
      */
     public function getQueryString($params = [])
     {
         $request = pluginApp(Request::class);
 
+        // FIX use $request->query() instead of $request->all() to avoid appending params from request body while rendering twig via POST calls, e.g. via the shop builder
         $queryParameters = $request->query();
         unset($queryParameters['plentyMarkets']);
         $queryParameters = array_replace($queryParameters, $params);
