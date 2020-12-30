7ace54a09291a7834e183cac0c6262ef80814014
Felix Gehrmann <felix.gehrmann@plentymarkets.com>
2020-12-30 10:57:18 +0100

    docs and psr12 customernewsletterservice

--- src/Services/CustomerNewsletterService.php
+++ src/Services/CustomerNewsletterService.php
@@ -2,7 +2,6 @@
 
 namespace IO\Services;
 
-use Illuminate\Http\Request;
 use Plenty\Modules\Account\Newsletter\Contracts\NewsletterRepositoryContract;
 use Plenty\Modules\Account\Newsletter\Models\Recipient;
 use Plenty\Modules\Authorization\Services\AuthHelper;
@@ -30,10 +29,12 @@ class CustomerNewsletterService
     }
 
     /**
-     * @param $email
-     * @param $emailFolder
-     * @param string $firstName
-     * @param string $lastName
+     * Subscribes a single user to a newsletter
+     * @param string $email The email address of the customer
+     * @param int $emailFolder Id for the type of newsletter
+     * @param string $firstName Optional: First name of the customer
+     * @param string $lastName Optional: Last name of the customer
+     * @throws \Throwable
      */
     public function saveNewsletterData($email, $emailFolder, $firstName = '', $lastName = '')
     {
@@ -41,43 +42,39 @@ class CustomerNewsletterService
     }
 
     /**
-     * @param $email
-     * @param $emailFolders
-     * @param string $firstName
-     * @param string $lastName
+     * Subscribes a single user to multiple newsletters
+     * @param string $email The email address of the customer
+     * @param int[] $emailFolders Ids for the types of newsletters
+     * @param string $firstName Optional: First name of the customer
+     * @param string $lastName Optional: Last name of the customer
      * @throws \Throwable
      */
     public function saveMultipleNewsletterData($email, $emailFolders, $firstName = '', $lastName = '')
     {
-        if (strlen($email) && count($emailFolders))
-        {
+        if (strlen($email) && count($emailFolders)) {
             /** @var AuthHelper $authHelper */
             $authHelper = pluginApp(AuthHelper::class);
             $newsletterRepo = $this->newsletterRepo;
 
-            foreach ($emailFolders as $key => $emailFolder)
-            {
-                $recipientData = $authHelper->processUnguarded(function() use ($email, $emailFolder, $newsletterRepo)
-                {
+            foreach ($emailFolders as $key => $emailFolder) {
+                $recipientData = $authHelper->processUnguarded(function () use ($email, $emailFolder, $newsletterRepo) {
                     return $newsletterRepo->listRecipients(['*'], 1, 1, ['email' => $email, 'folderId' => $emailFolder], [])->getResult()[0];
                 });
 
-                if ($recipientData instanceof Recipient)
-                {
+                if ($recipientData instanceof Recipient) {
                     unset($emailFolders[$key]);
                 }
             }
 
-            if (count($emailFolders))
-            {
+            if (count($emailFolders)) {
                 $this->newsletterRepo->addToNewsletterList($email, $firstName, $lastName, $emailFolders);
             }
         }
     }
 
     /**
-     * @param $authString
-     * @param $newsletterEmailId
+     * @param string $authString Authorization string used for security purposes
+     * @param int $newsletterEmailId Unique id of the newsletter registration
      * @return bool
      * @throws \Throwable
      */
@@ -87,15 +84,12 @@ class CustomerNewsletterService
         $authHelper = pluginApp(AuthHelper::class);
         $newsletterRepo = $this->newsletterRepo;
 
-        $emailData = $authHelper->processUnguarded(function() use ($newsletterEmailId, $newsletterRepo)
-        {
+        $emailData = $authHelper->processUnguarded(function () use ($newsletterEmailId, $newsletterRepo) {
             return $newsletterRepo->listRecipientById($newsletterEmailId);
         });
 
-        if ($authString === $emailData->confirmAuthString)
-        {
-            $authHelper->processUnguarded(function() use ($newsletterEmailId, $newsletterRepo)
-            {
+        if ($authString === $emailData->confirmAuthString) {
+            $authHelper->processUnguarded(function () use ($newsletterEmailId, $newsletterRepo) {
                 $newsletterRepo->updateRecipientById($newsletterEmailId, [
                     'confirmedTimestamp' => date('Y-m-d H:i:s', time())
                 ]);
@@ -110,9 +104,9 @@ class CustomerNewsletterService
     /**
      * Delete recipients from the newsletter
      * Not passing the optional parameter $emailFolder deletes the recipient from all email-folders
-     * @param string $email
-     * @param int $emailFolder
-     * @return bool $success
+     * @param string $email Email of the user
+     * @param int $emailFolder Id of the type of newsletter
+     * @return bool
      * @throws \Throwable
      */
     public function deleteNewsletterDataByEmail($email, $emailFolder = 0)
@@ -126,35 +120,27 @@ class CustomerNewsletterService
             'email' => $email
         ];
 
-        if ($emailFolder > 0)
-        {
+        if ($emailFolder > 0) {
             $filter['folderId'] = $emailFolder;
         }
 
         // Fetch list of \Recipient based on filters
-        $recipientList = $authHelper->processUnguarded(function() use ($filter, $newsletterRepo)
-        {
+        $recipientList = $authHelper->processUnguarded(function () use ($filter, $newsletterRepo) {
             return $newsletterRepo->listRecipients(['*'], 1, 100, $filter, [])->getResult();
         });
 
         // If any exist, delete them
-        if (count($recipientList))
-        {
-            foreach ($recipientList as $recipientData)
-            {
-                if ($recipientData instanceof Recipient)
-                {
-                    $authHelper->processUnguarded(function() use ($recipientData, $newsletterRepo)
-                    {
+        if (count($recipientList)) {
+            foreach ($recipientList as $recipientData) {
+                if ($recipientData instanceof Recipient) {
+                    $authHelper->processUnguarded(function () use ($recipientData, $newsletterRepo) {
                         return $this->newsletterRepo->deleteRecipientById($recipientData->id);
                     });
                 }
             }
 
             $success = true;
-        }
-        else
-        {
+        } else {
             $success = false;
         }
 
