32ae5d5fea3ec4711642bf2198cf05ef6f3a678c
cjohannsen <christoph.johannsen@plentymarkets.com>
2021-01-19 17:15:56 +0100

    CHANGE ContextInterface.php - add missing code comments

--- src/Helper/ContextInterface.php
+++ src/Helper/ContextInterface.php
@@ -2,7 +2,14 @@
 
 namespace IO\Helper;
 
+/**
+ * Interface for building context classes. These classes are used to pass data from PHP into TWIG.
+ */
 interface ContextInterface
 {
+    /**
+     * Initialize the context and set the properties.
+     * @param array Passthrough variables from the controller.
+     */
     public function init($params);
 }
