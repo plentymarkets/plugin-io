861532d0f9528a86d2d41540ed89b39ab5cfddab
Steve Tentrop <steve.tentrop@plentymarkets.com>
2021-02-15 16:58:50 +0100

    add  documentation for config

--- src/Config/IOConfig.php
+++ src/Config/IOConfig.php
@@ -2,11 +2,23 @@
 
 namespace IO\Config;
 
+/**
+ * Class IOConfig
+ *
+ * IO Configuriaton data
+ *
+ * @package IO\Config
+ */
 class IOConfig
 {
-    /** @var IONumberFormatConfig */
+    /**
+     * @var IONumberFormatConfig IO Configuration data for number format.
+     */
     public $format;
 
+    /**
+     * IOConfig constructor, load sub configuration data.
+     */
     public function __construct()
     {
         $this->format = pluginApp(IONumberFormatConfig::class);
