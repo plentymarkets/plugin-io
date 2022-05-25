b8c4f0f9b96f205056224c04a0bfa350de9ff311
Jonas Böwer <jonas.boewer@plentymarkets.com>
2022-05-25 08:10:47 +0200

    Fix reduce func

--- src/Services/ItemSearch/Extensions/ReduceDataExtension.php
+++ src/Services/ItemSearch/Extensions/ReduceDataExtension.php
@@ -32,7 +32,7 @@ class ReduceDataExtension implements ItemSearchExtension
      */
     public function transformResult($baseResult, $extensionResult)
     {
-        if( count($baseResult['documents'] ) )
+        if(is_array($baseResult['documents']) && count($baseResult['documents']))
         {
             $baseResult['documents'] = array_map(function($document)
             {
@@ -46,6 +46,10 @@ class ReduceDataExtension implements ItemSearchExtension
 
     private function reduceData($data)
     {
+        if (!is_array($data['properties'])) {
+            return $data;
+        }
+
         $properties = array_filter($data['properties'], function($property)
         {
             return $property['property']['isShownOnItemPage']
