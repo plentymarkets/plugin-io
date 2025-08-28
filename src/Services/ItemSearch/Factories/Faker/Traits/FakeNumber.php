520458f2eb26611c0eb3311e1cef21c9e4cba0e3
Steve Tentrop <steve.tentrop@plentyone.com>
2025-08-28 16:22:56 +0200

    add check for random_int

--- src/Services/ItemSearch/Factories/Faker/Traits/FakeNumber.php
+++ src/Services/ItemSearch/Factories/Faker/Traits/FakeNumber.php
@@ -6,8 +6,7 @@ trait FakeNumber
 {
     protected function number($min = 0, $max = 999999)
     {
-        if ( $max < $min )
-        {
+        if ($max < $min) {
             $max = 999999;
         }
 
@@ -16,7 +15,8 @@ trait FakeNumber
 
     protected function float($min = 0, $max = 9999, $numberOfDecimals = 2)
     {
-        $digit    = random_int($min + 1, $max);
+        $min = $min + 1;
+        $digit = random_int($min, max($max, $min));
         $tmp      = (random_int(0, mt_getrandmax()) / random_int(0, mt_getrandmax()));
         $fraction = $tmp - ($tmp % 10);
         $result   = intval((($digit - 1) + $fraction) * $this->pow(10, $numberOfDecimals));
@@ -32,14 +32,12 @@ trait FakeNumber
     private function pow($base, $exp)
     {
         $result = 1;
-        while($exp > 0)
-        {
+        while ($exp > 0) {
             $result *= $base;
             $exp--;
         }
 
-        while($exp < 0)
-        {
+        while ($exp < 0) {
             $result /= $base;
             $exp++;
         }
