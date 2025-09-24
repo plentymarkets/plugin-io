66e7a49116c7185693a834067c02947f31610936
Fabian Gerke <124085586+FabianGerke@users.noreply.github.com>
2025-09-24 09:40:24 +0200

    undo

--- src/Services/BasketService.php
+++ src/Services/BasketService.php
@@ -201,12 +201,17 @@ class BasketService
                 ]
             );
 
-            if (
-                $this->couponService->effectsOnShippingCosts($campaign) &&
-                !$basket['shippingDeleteByCoupon']
-            ) {
-                $basket['shippingAmountNet'] -= abs($couponValidation->shippingDiscountNet);
-                $basket['shippingAmount'] -= abs($couponValidation->shippingDiscount);
+            if ($this->couponService->effectsOnShippingCosts($campaign) && !$basket['shippingDeleteByCoupon']) {
+                if ($couponValidation->shippingDiscountNet < 0) {
+                    $basket['shippingAmountNet'] += $couponValidation->shippingDiscountNet;
+                } else {
+                    $basket['shippingAmountNet'] -= $couponValidation->shippingDiscountNet;
+                }
+                if ($couponValidation->shippingDiscount < 0) {
+                    $basket['shippingAmount'] += $couponValidation->shippingDiscount;
+                } else {
+                    $basket['shippingAmount'] -= $couponValidation->shippingDiscount;
+                }
             }
         }
 
