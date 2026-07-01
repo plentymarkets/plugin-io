b051cce718327a17202bfb42bfe6731be99b49ba
N-Feist <noah.feist@plentyone.com>
2026-07-01 11:23:09 +0200

    test: send data to BE unprocessed

--- src/Api/Resources/CancellationResource.php
+++ src/Api/Resources/CancellationResource.php
@@ -5,10 +5,7 @@ namespace IO\Api\Resources;
 use IO\Api\ApiResource;
 use IO\Api\ApiResponse;
 use IO\Api\ResponseCode;
-use IO\Constants\LogLevel;
-use IO\Helper\ReCaptcha;
 use IO\Helper\Utils;
-use IO\Services\NotificationService;
 use Plenty\Modules\Webshop\Storefront\Contracts\CancellationRepositoryContract;
 use Plenty\Modules\Webshop\Storefront\Exceptions\StorefrontException;
 use Plenty\Plugin\Http\Request;
@@ -50,46 +47,20 @@ class CancellationResource extends ApiResource
     {
         try {
             $requestData = $this->request->all();
-            $formData = $requestData['data'] ?? [];
-            $errors = [];
+            $requestData['lang'] = Utils::getLang();
 
-            if(empty($formData)){
-                return $this->response->create(false, ResponseCode::BAD_REQUEST);
-            }
+            $responseData = $this->cancellationRepository->submitCancellationRequest($requestData);
 
-            $formData = array_change_key_case($formData);
-            foreach (['email', 'name', 'order'] as $field) {
-                if(empty($formData[$field]['value'])) {
-                    $errors[] = $field;
-                }
-            }
-
-            if(!empty($errors)) {
-                $message = 'Keys "' . implode('", "', $errors) . '" of the contract withdrawal form couldn\'t be mapped to the email template.';
-
-                $this->getLogger(self::class)->error(
-                    "IO::Debug.CancellationResource_missingFields",
-                    [
-                        "code" => ResponseCode::BAD_REQUEST,
-                        "message" => $message
-                    ]
-                );
-
-                return $this->response->create(false, ResponseCode::BAD_REQUEST);
-            }
-
-            $this->cancellationRepository->submitCancellationRequest([
-                'email' => $formData['email']['value'],
-                'name' => $formData['name']['value'],
-                'lang' => Utils::getLang(),
-                'orderId' => $formData['order']['value'],
-                'reason' => $formData['reason']['value'] ?? '',
-                'recipient' => $requestData['recipient'] ?? '',
-            ]);
-
-            return $this->response->create(true, ResponseCode::OK);
-        } catch (\Exception $exception) {
+            return $this->response->create($responseData, ResponseCode::OK);
+        } catch (StorefrontException $exception) {
             $code = $exception->getCode() ?: ResponseCode::INTERNAL_SERVER_ERROR;
+
+            return $this->response->create([
+                'error' => $exception->getMessage()
+            ], $code);
+        } catch (\Throwable $exception) {
+            $code = $exception->getCode() ?: ResponseCode::INTERNAL_SERVER_ERROR;
+
             return $this->response->create(false, $code);
         }
     }
